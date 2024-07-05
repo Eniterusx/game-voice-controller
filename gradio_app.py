e10e0ade11540c2fec62e026ad1b08d2116cec10
Jakub Kot <jakub02kot@gmail.com>
2024-07-05 03:30:25 +0200

    added model

---
+++ gradio_app.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from torchvision import models
+
+import gradio as gr
+import os
+import numpy as np
+from torchvision import transforms
+
+from bcresnet import BCResNets
+from utils import DownloadDataset, Padding, Preprocess, SpeechCommand, SplitDataset
+
+# Load the model
+class Model:
+    def __init__(self):
+        self.model = BCResNets(int(self.tau * 8)).to(self.device)
+        self.model.load_state_dict(torch.load("model.pth", map_location=self.device))       
+
+def classify():
+    pass 
+
+# create a gradio interface that accepts audio input as microphone or wav file and returns the predicted class
+interface = gr.Interface(
+    fn=classify,
+    inputs=gr.Audio(),
+    outputs="label",
+    title="Speech Command Recognition",
+    description="Recognize speech commands using a ResNet model.",
+    theme="huggingface",
+)
+interface.launch()
